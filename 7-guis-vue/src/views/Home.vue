ff8a5fccfe8fa5379fb1ac652eef191ee9fec717
Bruna Nallis <bruna.gnallis@gmail.com>
2023-11-12 12:37:26 -0300

    home feita

--- 7-guis-vue/src/views/Home.vue
+++ 7-guis-vue/src/views/Home.vue
@@ -1,19 +1,59 @@
 <template>
-  <div class="flex h-screen">
-    <aside class="bg-gray-800 text-white p-4 w-1/4 fixed h-full">
+  <div class="flex h-screen bg-blue-100">
+    <aside class="bg-blue-200 text-gray-800 p-4 w-1/5 max-h-screen">
+      <!-- Conteúdo da barra lateral -->
       <h2 class="text-xl font-semibold mb-4">Menu</h2>
       <ul>
-        <li><router-link to="/cells">Cells</router-link></li>
-        <li><router-link to="/circle-drawer">Circle Drawer</router-link></li>
-        <li><router-link to="/counter">Counter</router-link></li>
-        <li><router-link to="/crud">Crud</router-link></li>
-        <li><router-link to="/flight">Flight</router-link></li>
-        <li><router-link to="/temperature-convert">Temperature Convert</router-link></li>
-        <li><router-link to="/timer">Timer</router-link></li>
+        <li>
+          <router-link class="hover:text-blue-500" to="/cells"
+            >Cells</router-link
+          >
+        </li>
+        <li>
+          <router-link class="hover:text-blue-500" to="/circle-drawer"
+            >Circle Drawer</router-link
+          >
+        </li>
+        <li>
+          <router-link class="hover:text-blue-500" to="/counter"
+            >Counter</router-link
+          >
+        </li>
+        <li>
+          <router-link class="hover:text-blue-500" to="/crud">Crud</router-link>
+        </li>
+        <li>
+          <router-link class="hover:text-blue-500" to="/flight"
+            >Flight</router-link
+          >
+        </li>
+        <li>
+          <router-link class="hover:text-blue-500" to="/temperature-convert"
+            >Temperature Convert</router-link
+          >
+        </li>
+        <li>
+          <router-link class="hover:text-blue-500" to="/timer"
+            >Timer</router-link
+          >
+        </li>
       </ul>
     </aside>
 
-    <main class="flex-grow p-8 ml-1/4">
+    <main class="flex-grow p-8 ml-1/5">
+      <!-- Conteúdo principal aqui -->
+      <div>
+        <h1 class="text-3xl font-semibold mb-4">Bem-vindo à sua aplicação GUI!</h1>
+        <p class="text-gray-600 mb-6">
+          As tarefas nesta aplicação foram selecionadas com base em critérios rigorosos.
+          O conjunto de tarefas foi projetado para ser o menor possível, mas ainda reflete
+          uma ampla gama de desafios típicos na programação de interfaces gráficas do usuário (GUI).
+          Cada tarefa é concebida para ser simples e independente, evitando artificialidades excessivas.
+          Preferencialmente, cada tarefa é derivada de exemplos existentes, proporcionando uma
+          justificativa robusta para sua utilidade, além de já contar com pelo menos uma implementação de referência.
+        </p>
+      </div>
+      <!-- Outros elementos da home -->
       <router-view></router-view>
     </main>
   </div>
@@ -24,4 +64,3 @@ export default {
   name: 'Home'
 }
 </script>
-
